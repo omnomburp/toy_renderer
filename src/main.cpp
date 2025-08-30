2f9c814b26673a4114b12b121c09112dd075660a
jc <jeremychiambx@gmail.com>
2025-08-30 22:27:27 +0800

    Bresenham's line algo

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
-#include "tgaimage.h"
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+
+#include "tgaimage.h"
 
 constexpr TGAColor white =  {.bgra = {255, 255, 255, 255 }};
 constexpr TGAColor green =  {.bgra = {0, 255, 0, 255 }};
@@ -15,37 +16,33 @@ inline void swap(int &a, int &b) {
     b = temp;
 }
 
-inline int ret_abs(int x) {
-    if (x < 0) {
-        return -x;
-    }
-
-    return x;
-}
-
 inline void draw_line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
-    auto steep = ret_abs(ax - bx) < ret_abs(ay - by);
+    auto steep = std::abs(ax - bx) < std::abs(ay - by);
 
     if (steep) {
         swap(ax, ay);
         swap(bx, by);
     }
 
-
     if (ax > bx) {
         swap(ax, bx);
     }
 
-    for (int x = ax; x < bx; ++x) {
-        float t = (x - ax) / static_cast<float>((bx - ax));
+    int y = ay;
+    int error = 0;
 
-        int x_coord = std::round(ax + t*(bx - ax));
-        int y_coord = std::round(ay + t*(by - ay));
-        if (!steep)
-            framebuffer.set(x_coord, y_coord, color);
+    for (int x = ax; x <= bx; ++x) {
+
+        if (steep)
+            framebuffer.set(y, x, color);
         else
-            framebuffer.set(y_coord, x_coord, color);
+            framebuffer.set(x, y, color);
 
+        error += 2 * std::abs(by - ay);
+
+        const int mask = error > bx - ax;
+        y += (by > ay ? 1: -1) * mask;
+        error -= 2 * (bx - ax) * mask;
     }
 }
 
@@ -56,7 +53,7 @@ int main() {
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
     std::srand(std::time({}));
-    for (int i=0; i<(1<<24); i++) {
+    for (int i = 0; i < (1 << 24); i++) {
         int ax = rand()%width, ay = rand()%height;
         int bx = rand()%width, by = rand()%height;
         draw_line(ax, ay, bx, by, framebuffer, { static_cast<uint8_t>(rand()%255), static_cast<uint8_t>(rand()%255), static_cast<uint8_t>(rand()%255), static_cast<uint8_t>(rand()%255) });
